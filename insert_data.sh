388ee135e246484849ffab98466f2edbf3dd9d94
AT2112 <129541007+AT2112@users.noreply.github.com>
2023-09-11 17:03:53 +0200

    Update insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -7,8 +7,6 @@ else
   PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
 fi
 
-# Do not change code above this line. Use the PSQL variable above to query your database.
-
 $PSQL "TRUNCATE TABLE teams, games;"
 cat games.csv | while IFS=',' read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
 do
